b0c1fbcdb386c885deaa2202f9257492badd2eac
Damir <damir@withpulp.com>
2015-02-18 22:12:30 -0500

    added vanilla meteor test

---
+++ xdate-test-vanilla/client/views/form.js
@@ -0,0 +1,10 @@
+Template['form'].helpers({
+	'date': function () {
+		var now = moment();
+		var later = now.clone().add(1, 'y');
+		return {
+			issued: now.toDate(),
+			expires: later.toDate()
+		};
+	}
+});
